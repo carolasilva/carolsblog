792e47c48b4e1bd4844df6d86c15dc6b97910fef
Carol <carol.alves99@live.com>
2019-06-16 09:38:22 -0300

    Agora vai

---
+++ js/fakedb.js
@@ -0,0 +1,251 @@
+// declara um conjunto fake de dados para contatos
+var dbfake = {
+    "data": [
+        {
+            "id": 1,
+            "data": "13/06/2019 - 07h42",
+            "titulo": "Tite alivia tensão com vitória na estreia",
+            "texto": `Ainda como reflexo da eliminação nas quartas de final da Copa do Mundo de 2018 e das atuações pouco convincentes desde então, Tite iniciou a Copa América pressionado. Mesmo o técnico tendo um retrospecto de 29 vitórias (agora 30), cinco empates e apenas duas derrotas. 
+            Se a atuação na vitória por 3 a 0 sobre a Bolívia, na última sexta-feira, na estreia do torneio não foi de encher os olhos ou empolgar, ao menos amenizou o peso que o treinador carrega. Talvez, apenas o título da competição tire totalmente essa pressão, mas ainda é cedo.
+            Palco da estreia na Copa América, o Morumbi não é o melhor ambiente para um técnico de seleção brasileira que está pressionado por resultados. Historicamente, a torcida paulista é exigente com a Seleção (relembre aqui). Tanto que Tite dividiu opiniões antes de a bola rolar.
+            Quando o nome do treinador foi anunciado pelo sistema de som, logo depois da escalação titular e dos reservas, alguns torcedores vaiaram, e outros aplaudiram. Após a partida, Tite foi questionado sobre as vaias e como tem se sentido após mais uma estreia.
+            - Meu perfil não é ser incisivo. Sou direto. Mas não sou de fazer cena. Eu sou muito direto naquilo que tem de ser colocado. Estou mais cascudo. Absorvi um pouco mais (depois do tropeço na Copa do Mundo). Estou mais cascudo agora - falou o treinador.`,
+            "img": "carol123",
+            "curtidas": "60",
+            "curtido": false,
+            "likeColor": "black",
+            "comentarios": [
+                {
+                    "nome": "Tite",
+                    "texto": "Eu sou muito bom mesmo!!!"
+                },
+                {
+                    "nome": "Lucas",
+                    "texto": "Técnico horrível"
+                },
+                {
+                    "nome": "Marcos",
+                    "texto": "Vai Brasil!"
+                },
+                {
+                    "nome": "José",
+                    "texto": "Até parece que vamos ganhar essa Copa"
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "data": "13/06/2019 - 07h55",
+            "titulo": "Cidades brasileiras têm paralisação e protestos nesta sexta-feira",
+            "texto": `Cidades brasileiras registraram protestos e paralisações em serviços públicos nesta sexta-feira (14) contra a reforma da Previdência e os cortes na educação. Os 26 estados e o Distrito Federal foram afetados.
+            O principal efeito da greve foi no transporte coletivo. Ao longo da manhã, os efeitos da paralisação foram sentidos com a interrupção total ou parcial de linhas. Das 27 capitais, 19 tiveram o sistema de ônibus afetado pela mobilização. Considerando outros modais, como trem e metrô, esse número chegou a 21.
+            Outras oito capitais não tiveram interrupção no transporte coletivo por ônibus, mas sofreram com bloqueios de ruas ou estradas por manifestantes ou tiveram paralisação parcial no metrô — caso de São Paulo e Belo Horizonte, por exemplo (veja detalhes por estado mais abaixo).`,
+            "img": "carol123",
+            "curtidas": "1",
+            "curtido": false,
+            "likeColor": "black",
+            "comentarios": [
+                {
+                    "nome": "Eduardo",
+                    "texto": "Só atrapalham essas paralizações :( "
+                },
+                {
+                    "nome": "Carla",
+                    "texto": "Nem consegui ir no serviço"
+                },
+                {
+                    "nome": "Joana",
+                    "texto": "Ô BOLSONARO, VA SE *****, OS ESTUDANTES NÃO TÊM MEDO DE VOCÊ"
+                },
+                {
+                    "nome": "Thiago",
+                    "texto": "Nossa JOana, sua exaltada"
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "data": "14/06/2019 - 08h14",
+            "titulo": "Trio é preso após roubar R$ 688 mil em joias em salão de luxo",
+            "texto": `A Polícia Militar de Campinas (SP) prendeu na noite desta sexta-feira (14) duas mulheres e um homem suspeitos de roubar um salão de beleza de luxo, no bairro Nova Campinas. De acordo com a corporação, o trio fez um arrastão no local no momento em que ocorria uma exposição de joias. Os policiais conseguiram recuperar as peças, avaliadas em R$ 688 mil.
+            Um funcionário do salão contou à EPTV, afiliada TV Globo, que as duas mulheres que participaram do assalto visitaram o estabelecimento mais cedo para marcar um horário, como se fossem clientes, olhando, inclusive, mostruários de joias.
+            Segundo a testemunha, uma delas voltou ao local no final da tarde, acompanhada de um homem, e eles anunciaram o assalto. Além das joias, os criminosos fizeram um "arrastão" e roubaram celulares de clientes e materiais do salão.`,
+            "img": "carol123",
+            "curtidas": "6",
+            "curtido": false,
+            "likeColor": "black",
+            "comentarios": [
+                {
+                    "nome": "Carol",
+                    "texto": "Que loucura!"
+                },
+                {
+                    "nome": "Rafael",
+                    "texto": "Como esse povo consegue?"
+                }
+            ]
+        },
+        {
+            "id": 4,
+            "data": "14/06/2019 - 11h23",
+            "titulo": "Como fazer o seu dia mais produtivo",
+            "texto": `O alarme toca. Você automaticamente pressiona a “soneca”. O alarme toca mais umas duas vezes, sua mãe vem te chamar, até você finalmente acordar e ver que está atrasado para a aula. Você levanta correndo enquanto checa o WhatsApp. Lava o rosto, escova os dentes e faz maratona para vestir as roupas. Engole o café da manhã ainda meio atordoado, enquanto dá uma espiada no Facebook. Sai correndo porta afora para não perder o ônibus. Na aula, se distrai com tudo e fica morrendo de sono… Um saco, né?
+            Essa rotina é normal para muita gente que acorda cedo. Quando eu estava no colégio, fazia exatamente a mesma coisa: dormia até o último minuto, depois saía correndo para a aula sem nem saber que dia era. Mas, depois de alguns anos, comecei a perceber a importância de acordar bem, isto é, reservar algum tempo pela manhã para dar ao seu corpo a chance de despertar completamente e ficar disposto para enfrentar o resto do dia. Sei que é quase impossível pensar em perder quaisquer minutos de sono, mas pode acreditar: mudar alguns hábitos na sua manhã pode melhorar o seu dia em 1000%, aumentando até mesmo a sua concentração e capacidade de estudar.`,
+            "img": "carol123",
+            "curtidas": "15",
+            "curtido": false,
+            "likeColor": "black",
+            "comentarios": [
+                {
+                    "nome": "Quirino",
+                    "texto": "VocÊ não torna "
+                },
+                {
+                    "nome": "Alex",
+                    "texto": "Esse blog já foi melhor..."
+                },
+                {
+                    "nome": "Karina",
+                    "texto": "Parabéns pelo blog! Conteúdo super diversificado"
+                },
+                {
+                    "nome": "Jonas",
+                    "texto": "Quirino tá bem revoltado"
+                }
+            ]
+        },
+        {
+            "id": 5,
+            "data": "15/06/2019 - 09h23",
+            "titulo": "Como tirar 100 na faculdade",
+            "texto": `Em matéria de application, há uma série de termos comuns entre as exigências. Por exemplo, os testes padronizados, como TOEFL e SAT, solicitados pelas universidades estrangeiras. Junte-se a isso a lista de requisitos que não são tão óbvios ou fáceis de explicar, como o bom desempenho acadêmico.
+            É comum que editais para bolsas de estudo e candidaturas comuns, para graduação ou pós-graduação, mencionem esse critério na descrição do processo seletivo. Mais do que significar simplesmente “boas notas” na formação anterior, um bom desempenho acadêmico se traduz em vários aspectos da application.
+            Para começo de conversa, é necessário entender de que forma as notas são avaliadas pelas instituições de ensino. O histórico escolar é enviado à universidade, geralmente acompanhado de uma tradução para língua estrangeira, listando quais matérias um estudante cursou e quais as notas obtidas. 
+            Nesse momento, as universidades consideram não só as notas separadamente, como também uma média – o que, nas instituições americanas, é chamado de GPA. O resultado pode ser calculado com base em todas as matérias, ou com base em disciplinas tidas como essenciais.`,
+            "img": "carol123",
+            "curtidas": "23",
+            "curtido": false,
+            "likeColor": "black",
+            "comentarios": [
+                {
+                    "nome": "Rommel",
+                    "texto": "Na minha matéria não é fácil assim não"
+                }
+            ]
+        }
+    ]
+}
+
+// Caso exista no Local Storage, recupera os dados salvos
+var db = JSON.parse(localStorage.getItem('db'));
+if (!db) {
+    db = dbfake;
+    localStorage.setItem('db',JSON.stringify(dbfake));
+};
+
+// Exibe mensagem em um elemento de ID msg
+function displayMessage(msg) {
+    $('#msg').html('<div class="alert alert-warning">' + msg + '</div>');
+}
+
+function insertPostagem(postagem) 
+{
+    // Calcula novo Id a partir do último código existente no array
+    let novoId = db.data[db.data.length - 1].id + 1;
+    let data = new Date();
+    // Guarda cada pedaço em uma variável
+    let dia     = data.getDate();           // 1-31
+    let mes     = data.getMonth();          // 0-11 (zero=janeiro)
+    let ano    = data.getFullYear();       // 4 dígitos
+    let hora    = data.getHours();          // 0-23
+    let min     = data.getMinutes();        // 0-59
+
+    // Formata a data e a hora (note o mês + 1)
+    let str_data = dia + '/' + (mes+1) + '/' + ano;
+    let str_hora = hora + 'h' + min;
+    let data_completa = str_data + ' ' + str_hora;
+
+    let novaPostagem = {
+        "id": novoId,
+        "data": data_completa,
+        "titulo": postagem.titulo,
+        "texto": postagem.texto,
+        "imagens": postagem.img,
+        "curtidas": "0",
+        "curtido": false,
+        "likeColor": "black",
+        "comentarios": []
+    };
+
+    // Insere o novo objeto no array
+    db.data.push(novaPostagem);
+    displayMessage("Postagem inserida com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db', JSON.stringify(db));
+}
+
+function insertComentario (id, comentario)
+{
+    let index = db.data.map(obj => obj.id).indexOf(id);
+
+    db.data[index].comentarios.push(comentario);
+    localStorage.setItem('db', JSON.stringify(db));
+}
+
+function updateCurtida (id, curtida, curtido, color)
+{
+    let index = db.data.map(obj => obj.id).indexOf(id);
+
+    db.data[index].titulo = db.data[index].titulo,
+    db.data[index].texto = db.data[index].texto,
+    db.data[index].img = db.data[index].img,
+    db.data[index].curtidas =parseInt( db.data[index].curtidas) + curtida;
+    db.data[index].curtido = curtido;
+    db.data[index].likeColor = color;
+    db.data[index].comentarios =  db.data[index].comentarios;
+
+    localStorage.setItem('db', JSON.stringify(db));
+
+}
+function updatePostagem(id, postagem) {
+    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+    let index = db.data.map(obj => obj.id).indexOf(id);
+
+    // Altera os dados do objeto no array
+    db.data[index].titulo = postagem.titulo,
+    db.data[index].texto = postagem.texto,
+    db.data[index].img = postagem.img,
+    db.data[index].curtidas = postagem.curtidas,
+    db.data[index].comentarios = postagem.comentarios
+
+    displayMessage("Postagem alterada com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db', JSON.stringify(db));
+}
+
+function deletePostagem(id) {   
+    // Filtra o array removendo o elemento com o id passado
+    db.data = db.data.filter(function (element) { return element.id != id });
+
+    alert ("Postagem apagada com sucesso!");    
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db', JSON.stringify(db));
+}
+
+/*function procuraPostagem(postagem) {
+    
+    for (i = 0; i < db.data.length; i++) {
+        var  listaPostagem = db.data[i];
+        if (listaUsuario.usuario == procurado.usuario)
+        {
+            if (listaUsuario.senha == procurado.senha)
+                return 1;
+            else
+                return 2;
+        }
+    }
+    return 3;
+}*/
